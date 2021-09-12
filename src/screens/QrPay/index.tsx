6bd39fa5589fb61ddf5e065f175e604e7fb238bc
Edvaldo Afonso <eadafonso@gmail.com>
2021-09-12 08:53:35 +0100

    feat: make QR pay screen

--- src/screens/QrPay/index.tsx
+++ src/screens/QrPay/index.tsx
@@ -1,14 +1,63 @@
 import React from "react";
-import { View, Text } from "react-native";
 
-// import { Container } from './styles';
+import AppLoading from "expo-app-loading";
 
-const QrPay: React.FC = () => {
-  return (
-    <View>
-      <Text>QrPay</Text>
-    </View>
-  );
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_600SemiBold,
+  Inter_500Medium,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+
+import { TouchableOpacity } from "react-native";
+
+import { Text } from "../../components/Text";
+import { Feather } from "@expo/vector-icons";
+
+import { Container, Header, QrImage } from "./styles";
+
+const QrPay: React.FC = ({ navigation }: any) => {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_600SemiBold,
+    Inter_500Medium,
+    Inter_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <Header>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Feather name="chevron-left" color="#666" size={28} />
+          </TouchableOpacity>
+        </Header>
+
+        <Container>
+          <Text style={{ fontFamily: "Inter_900Black" }} extra>
+            Payment by QR code
+          </Text>
+
+          <Text
+            medium
+            margin="15px 0 20px 0"
+            color="#868686"
+            style={{ fontFamily: "Inter_500Medium", fontWeight: "100" }}
+          >
+            Tap the image to start scanning
+          </Text>
+
+          <QrImage source={require("../../../assets/logo.png")} />
+        </Container>
+      </>
+    );
+  }
 };
 
 export default QrPay;
---
+++ src/screens/QrPay/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const Header = styled.SafeAreaView`
+  height: 45px;
+  justify-content: center;
+  padding: 0 15px;
+  margin-top: 40px;
+`;
+
+export const Container = styled.View`
+  align-items: center;
+  margin-top: 35px;
+  padding: 0 24px;
+`;
+
+export const QrImage = styled.Image`
+  width: 150px;
+  height: 150px;
+  align-self: center;
+  margin-top: 65px;
+`;
